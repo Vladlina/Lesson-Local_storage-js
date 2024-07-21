e4343e2a2667fb73b2b3910112553591a799b90d
Vladlina <92329114+Vladlina@users.noreply.github.com>
2024-07-21 10:53:20 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,3 +6,22 @@
  *     <p>Текст</p>
  * </li>
  */
+
+// 1. Функція createTask, яка буде забирати дані та створювати 1 task.
+const form = document.querySelector('#task-form');
+
+form.addEventListener('submit', formSubmit);
+
+function formSubmit(e) {
+  e.preventDefault();
+
+  const taskName = e.target.elements.taskName.value.trim();
+  const taskText = e.target.elements.taskText.value.trim();
+  const newTask = {
+    taskName,
+    taskText,
+    id: Date.now(),
+  };
+
+  console.log(newTask);
+}
