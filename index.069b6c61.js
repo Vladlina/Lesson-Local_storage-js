2f8c6fe7266d8c62540e55b4e07115f56ebb0223
Serhii-Briushyn <serhii.briushyn@icloud.com>
2024-07-21 10:13:25 +0000

    Deploying to gh-pages from @ Vladlina/Lesson-Local_storage-js@58dc2e0c7efd0a71acdab1f4107fe06617227881 🚀

--- index.069b6c61.js
+++
@@ -1,2 +0,0 @@
-document.querySelector("#task-form").addEventListener("submit",(function(e){e.preventDefault();const t=e.target.elements.taskName.value.trim(),a=e.target.elements.taskText.value.trim(),n={taskName:t,taskText:a,id:Date.now()};console.log(n)}));
-//# sourceMappingURL=index.069b6c61.js.map
--- index.0b5f45e1.js
+++
@@ -1,2 +0,0 @@
-document.querySelector("#task-form").addEventListener("submit",(function(e){e.preventDefault();var t=e.target.elements.taskName.value.trim(),a=e.target.elements.taskText.value.trim(),n={taskName:t,taskText:a,id:Date.now()};console.log(n)}));
-//# sourceMappingURL=index.0b5f45e1.js.map
